5581a21853d6982a29d282081c973b76d5f8b3bb
Travis Cline <travis.cline@gmail.com>
2021-07-29 14:56:42 -0700

    examples: Clean up helloworld

--- examples/trivial/helloworld/helloworld.go
+++ examples/trivial/helloworld/helloworld.go
@@ -8,12 +8,10 @@ import (
 
 // Contract defines a trivial contract that checks that the Transaction is a single payment.
 func Contract(globals types.Globals, txn types.Transaction, gtxn types.TxGroup) (int, error) {
-	isSingleTx := globals.GroupSize == 3
-	// isPayment := txn.TypeEnum == types.PaymentTx
+	isSingleTx := globals.GroupSize == 1
 
-	// if isPayment && isSingleTx {
 	if isSingleTx {
-		return 2, nil
+		return 1, nil
 	}
 	return 0, fmt.Errorf("failed condition")
 }
