e3f4da5fa67a547199e0a9dd2d98c18293d10ed2
Travis Cline <travis.cline@gmail.com>
2021-08-14 16:25:30 -0700

    examples: Augment hello_world to cause a Phi node

--- examples/trivial/helloworld/helloworld.go
+++ examples/trivial/helloworld/helloworld.go
@@ -7,10 +7,12 @@ import (
 )
 
 var Foobar = 42
+var Foobaz = 43
 
 // Contract defines a trivial contract that checks that the Transaction is a single payment.
 func Contract(globals types.Globals, txn types.Transaction, gtxn types.TxGroup) (int, error) {
-	if Foobar == 42 {
+	x := Foobar == 42 && Foobaz == 43
+	if x {
 		return 2, nil
 	}
 	return 0, fmt.Errorf("failed condition")
